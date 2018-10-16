36ee953357c81a6e941f53ca5232bad7a02723a9
winir <44081810+winir@users.noreply.github.com>
2018-10-16 04:50:50 +0200

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,9 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import ServicesPagePreview from './preview-templates/ServicesPagePreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('services', ServicesPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
